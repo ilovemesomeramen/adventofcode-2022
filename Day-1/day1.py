ddfec3be0733da2a8e8c8244ccdc424d87553288
Lukas Erlbacher <lukas.erlbacher97@gmail.com>
2022-12-01 13:10:19 +0100

    Day-1: Basic Python solution

---
+++ Day-1/day1.py
@@ -0,0 +1,52 @@
+
+
+def part1():
+    max_val = 0
+    with open("input", "r") as f:
+        lines = f.readlines() + ["\n"]
+        o = 0
+        while True:
+            count = 0
+            if o == len(lines):
+                break
+            for i, line in enumerate(lines[o:]):
+                line = line.strip()
+                if line:
+                    count += int(line)
+                else:
+                    o += i + 1
+                    break
+            if count > max_val:
+                max_val = count
+    return max_val
+
+
+def part2():
+    top_3 = [0, 0, 0]
+    with open("input", "r") as f:
+        lines = f.readlines() + ["\n"]
+        o = 0
+        while True:
+            count = 0
+            if o == len(lines):
+                break
+            for i, line in enumerate(lines[o:]):
+                line = line.strip()
+                if line:
+                    count += int(line)
+                else:
+                    o += i + 1
+                    break
+            if count > top_3[2]:
+                top_3[2] = count
+            elif count > top_3[1]:
+                top_3[1] = count
+            elif count > top_3[0]:
+                top_3[0] = count
+            top_3.sort(reverse=True)
+    return sum(top_3)
+
+
+if __name__ == '__main__':
+    print(part1())
+    print(part2())
